83196734f595a84fa5def440dbf311d98b0cf226
1p6 <aaapf12@gmail.com>
2014-05-03 20:55:03 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function(){
+	$("#rslider, #gslider, #bslider").slider({
+		max: 255,
+		value: 255,
+		range: "min",
+		slide: slide,
+		change: slide
+	});
+	$("#rslider").slider("value", 50);
+});
+function slide()
+{
+	red=$("#rslider").slider("value");
+	green=$("#gslider").slider("value");
+	blue=$("#bslider").slider("value");
+	$("#r").innerHTML=red;
+	$("#g").innerHTML=green;
+	$("#b").innerHTML=blue;
+	$("#output").css("background-color", "rgb("+red+","+green+","+blue+")");
+}
